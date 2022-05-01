83ccadf59ef5da1c006625d6f6db6dc3fcf3bf26
Manasisaner <102273402+Manasisaner@users.noreply.github.com>
2022-05-01 21:39:39 +0530

    Add files via upload

---
+++ SquareRootOfNumber.cs
@@ -0,0 +1,13 @@
+﻿namespace NUnitTestingProject
+{
+    public class SquareRootOfNumber
+    {
+        public void SquareRoot()
+        {
+            Console.WriteLine("Enter the positive Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            double root = Math.Sqrt(number);
+            Console.WriteLine("square root of the number is :--->" + root);
+        }
+    }
+}
